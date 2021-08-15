a0ce84067b51ef00942a88637e963c57bf4a35dc
Lalit Kumar <lalitkumar.meena.lk@gmail.com>
2021-08-15 17:26:46 +0530

    inital commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,102 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect, useCallback } from 'react';
+import prsMergedSince from 'prs-merged-since';
+import { Octokit } from '@octokit/core';
+import debounce from 'lodash/debounce';
+import Box from '@material-ui/core/Box';
+import Typography from '@material-ui/core/Typography';
+import TextField from '@material-ui/core/TextField';
+import Button from '@material-ui/core/Button';
+
+import { useDebounce } from 'utils/customHooks';
 
 function App() {
+  const [prsData, setPrsData] = useState([]);
+  const [owner, setOwner] = useState(null);
+  const [repository, setRepository] = useState(null);
+  const [tag, setTag] = useState(null);
+  const debouncedOwner = useDebounce(owner, 1000);
+  const debouncedRepository = useDebounce(repository, 1000);
+  const octokit = new Octokit({ auth: process.env.TOKEN });
+
+  function handleChangeOwner(event) {
+    const {
+      target: { value },
+    } = event;
+    setOwner(value);
+  }
+
+  function handleRepositoryChange(event) {
+    const {
+      target: { value },
+    } = event;
+    setRepository(value);
+  }
+
+  function handleTagChange(event) {
+    const {
+      target: { value },
+    } = event;
+    setTag(value);
+  }
+
+  async function getData() {
+    if (debouncedOwner && debouncedRepository) {
+      const response = await octokit.request(
+        'GET /repos/{owner}/{repo}/pulls',
+        {
+          owner: debouncedOwner,
+          repo: debouncedRepository,
+          type: 'private',
+        },
+      );
+      console.log({ response });
+    }
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Box
+      display="flex"
+      flexDirection="column"
+      justifyContent="center"
+      alignItems="center"
+      margin="auto"
+      width={1}
+    >
+      <Typography variant="h5">PRs Merged Since</Typography>
+
+      <Box my={1}>
+        <TextField
+          variant="outlined"
+          label="Owner"
+          value={owner}
+          onChange={handleChangeOwner}
+          autofocus
+        />
+      </Box>
+
+      <Box my={1}>
+        <TextField
+          variant="outlined"
+          label="Repository"
+          value={repository}
+          onChange={handleRepositoryChange}
+          autofocus
+        />
+      </Box>
+      <Box my={1}>
+        <TextField
+          variant="outlined"
+          label="Tag"
+          value={tag}
+          onChange={handleTagChange}
+          autofocus
+        />
+      </Box>
+
+      <Button variant="outlined" onClick={getData}>
+        Get Data
+      </Button>
+    </Box>
   );
 }
 
